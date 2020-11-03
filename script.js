fb1bf07d1ebddab8575a02f8ae61df55adf8d753
Tosin-webdev <oladejit3@gmail.com>
2020-11-03 05:10:17 +0100

    removed unnecessary comment

--- script.js
+++ script.js
@@ -21,6 +21,7 @@ const weekdays = [
   "Friday",
   "Saturday",
 ];
+
 const giveaway = document.querySelector(".giveaway");
 const deadline = document.querySelector(".deadline");
 const items = document.querySelectorAll(".deadline-format h4");
@@ -30,12 +31,7 @@ let tempMonth = tempDate.getMonth();
 let tempYear = tempDate.getFullYear();
 let tempDay = tempDate.getDate();
 
-console.log(tempDate);
-console.log(tempYear);
-console.log(tempMonth);
-console.log(tempDay);
 const futureDate = new Date(tempYear, tempMonth, tempDay + 6, 8, 30, 0);
-console.log(futureDate);
 
 // let futureDate = new Date(2020, 3, 24, 11, 30, 0);
 
@@ -43,21 +39,15 @@ const year = futureDate.getFullYear();
 const hours = futureDate.getHours();
 const minutes = futureDate.getMinutes();
 
-console.log(minutes);
-new Date(2020);
-
 let month = futureDate.getMonth();
 month = months[month];
-console.log(month);
+
 const weekday = weekdays[futureDate.getDay()];
-console.log(weekday);
 const date = futureDate.getDate();
 giveaway.textContent = `giveaway ends on ${weekday}, ${date} ${month} ${year} ${hours}:${minutes}am`;
-// const present = futureDate.getDay();
-// console.log(present);
 
 const futureTime = futureDate.getTime();
-console.log(futureTime);
+
 function getRemaindingTime() {
   const today = new Date().getTime();
 
@@ -69,25 +59,25 @@ function getRemaindingTime() {
   // 1d = 24hr
   // values in miliseconds
   const oneDay = 24 * 60 * 60 * 1000;
-  console.log(oneDay);
+
   const oneHour = 60 * 60 * 1000;
-  console.log(oneHour);
+
   const oneMinute = 60 * 1000;
-  console.log(oneMinute);
+
   // calculate all values
   let days = t / oneDay;
-  console.log(days);
+
   days = Math.floor(days);
-  console.log(days);
+
   let hours = Math.floor((t % oneDay) / oneHour);
-  console.log(hours);
+
   let minutes = Math.floor((t % oneHour) / oneMinute);
-  console.log(minutes);
+
   let seconds = Math.floor((t % oneMinute) / 1000);
-  console.log(seconds);
+
   // set values array
   const values = [days, hours, minutes, seconds];
-  console.log(values);
+
   function format(item) {
     if (item < 10) {
       return (item = `0${item}`);
